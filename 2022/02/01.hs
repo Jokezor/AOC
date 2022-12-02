31668859d42a98f66d5cd3050139be61fb013a02
Jokezor <testifyingfor@gmail.com>
2022-12-02 07:53:21 +0100

    Refactor for short and concise, not more readable though.

--- 2022/02/01.hs
+++ 2022/02/01.hs
@@ -5,31 +5,20 @@ main = do
   s <- readFile "input.txt"
   let sLines = lines s
   let strategy = map (splitOn " ") sLines
-  let myTotScore = myScore strategy
-  print strategy
+  let myTotScore = sum (map calcOutcomeScore strategy)
   print myTotScore
-
-myScore :: [[String]] -> Int
-myScore strategy = foldl calcScore 0 strategy
 
 calcOutcomeScore :: [String] -> Int
 calcOutcomeScore move =
   case move of
-    ["A", "Y"] -> 6
-    ["B", "Z"] -> 6
-    ["C", "X"] -> 6
-    ["A", "X"] -> 3
-    ["B", "Y"] -> 3
-    ["C", "Z"] -> 3
+    ["A", "Y"] -> 8
+    ["B", "Z"] -> 9
+    ["C", "X"] -> 7
+    ["A", "X"] -> 4
+    ["B", "Y"] -> 5
+    ["C", "Z"] -> 6
+    [_, "X"] -> 1
+    [_, "Y"] -> 2
+    [_, "Z"] -> 3
     _ -> 0
 
-calcScore :: Int -> [String] -> Int
-calcScore totScore [opponent, me] = 
-  if me == "X" then 
-      totScore + (calcOutcomeScore [opponent, me]) + 1
-  else if me == "Y" then
-      totScore + (calcOutcomeScore [opponent, me]) + 2
-  else if me == "Z" then
-      totScore + (calcOutcomeScore [opponent, me]) + 3
-  else totScore
-  
--- 2022/02/02.hs
+++ 2022/02/02.hs
@@ -4,45 +4,23 @@ main :: IO ()
 main = do
   s <- readFile "input.txt"
   let sLines = lines s
-  let l = map (splitOn " ") sLines
-  let myTotScore = myScore l
-  print l
+  let strategy = map (splitOn " ") sLines
+  let myTotScore = sum (map calcOutcomeScore strategy)
   print myTotScore
 
-myScore :: [[String]] -> Int
-myScore strategy =  (foldl calcScore 0 strategy)
+calcOutcomeScore :: [String] -> Int
+calcOutcomeScore move =
+  case move of
+    ["A", "X"] -> 3
+    ["B", "X"] -> 1
+    ["C", "X"] -> 2
 
-calcScore :: Int -> [String] -> Int
-calcScore totScore [opponent, me] = 
-  if me == "X" then 
-      (if opponent == "A" then 
-          totScore + 3
-      else if opponent == "C" then 
-          totScore + 2
-      else if opponent == "B" then
-          totScore + 1
-      else
-          totScore
-      )
-  else if me == "Y" then
-      (if opponent == "A" then
-          totScore + 1 + 3
-       else if opponent == "B" then
-          totScore + 2 + 3
-       else if opponent == "C" then
-           totScore + 3 + 3
-       else
-           totScore
-      )
-  else if me == "Z" then
-      (if opponent == "A" then
-          totScore + 2 + 6
-       else if opponent == "B" then
-          totScore + 3 + 6
-       else if opponent == "C" then
-          totScore + 1 + 6
-       else 
-          totScore
-      )
-  else totScore
-  
+    ["A", "Y"] -> 4
+    ["B", "Y"] -> 5
+    ["C", "Y"] -> 6
+
+    ["A", "Z"] -> 8
+    ["B", "Z"] -> 9
+    ["C", "Z"] -> 7
+    _ -> 0
+
